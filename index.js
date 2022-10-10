9a018283d378fb0623e06778d5605e245ad71353
meankit2610 <ankitsingh5282770@gmail.com>
2022-10-10 14:35:23 +0530

    Api UpdateHeroku-port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const connection = require('./config/db')
 const newsRouter = require('./routes/news')
 const cors = require('cors');
-const PORT = 8080
+const PORT = process.env.PORT || 5000
 const app = express()
 app.use(express.json())
 app.use(cors());
