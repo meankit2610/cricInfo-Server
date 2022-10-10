b6f00e9e13876ef7c8cfca7cf6a31fa20a495583
meankit2610 <ankitsingh5282770@gmail.com>
2022-10-10 14:50:31 +0530

    Api UpdateHeroku-port

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
 
-const connection = mongoose.connect('mongodb://localhost:27017/cricinfo_news')
+const connection = mongoose.connect('mongodb+srv://ankit:ankit770@cluster0.n05qagf.mongodb.net/?retryWrites=true&w=majority')
 console.log("mongoose connect")
 module.exports = connection
